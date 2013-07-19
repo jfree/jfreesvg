967645a0d71af2efdeb4d06bfa3c0f311c5792eb
David Gilbert <david.gilbert@object-refinery.com>
2013-07-19 17:43:15 +0200

    Fill out this class for implementing referenced images in SVGGraphics2D.

--- src/main/java/org/jfree/graphics2d/svg/ImageElement.java
+++ src/main/java/org/jfree/graphics2d/svg/ImageElement.java
@@ -6,7 +6,8 @@ package org.jfree.graphics2d.svg;
 import java.awt.Image;
 
 /**
- * An image reference.
+ * An image reference.  This object provides the name (href) used in the
+ * SVG element to refer to an external image, and the Image itself.
  */
 public class ImageElement {
     
@@ -15,4 +16,34 @@ public class ImageElement {
     
     /** The image. */
     private Image image;
+    
+    /**
+     * Creates a new instance.
+     * 
+     * @param href  the href.
+     * @param image  the image.
+     */
+    public ImageElement(String href, Image image) {
+        this.href = href;
+        this.image = image;
+    }
+
+    /**
+     * Returns the href.
+     * 
+     * @return The href.
+     */
+    public String getHref() {
+        return href;
+    }
+
+    /**
+     * Returns the image.
+     * 
+     * @return The image. 
+     */
+    public Image getImage() {
+        return image;
+    }
+ 
 }
