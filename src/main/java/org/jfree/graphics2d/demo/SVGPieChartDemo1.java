29f0f06e5444b0903fffe17ab873623625ee6455
David Gilbert <david.gilbert@object-refinery.com>
2013-07-23 18:56:41 +0200

    Update for refactoring.

--- src/main/java/org/jfree/graphics2d/demo/SVGPieChartDemo1.java
+++ src/main/java/org/jfree/graphics2d/demo/SVGPieChartDemo1.java
@@ -95,6 +95,6 @@ public class SVGPieChartDemo1 {
         Rectangle r = new Rectangle(0, 0, 600, 400);
         chart.draw(g2, r);
         File f = new File("SVGPieChartDemo1.html");
-        writeToHTML(f, g2.getSVG());
+        writeToHTML(f, g2.getSVGElement());
     }
 }
