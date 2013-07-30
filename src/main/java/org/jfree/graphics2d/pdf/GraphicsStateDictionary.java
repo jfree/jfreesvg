f74b10e81a6949c16a7dfc6a081f8afb1502d28f
David Gilbert <david.gilbert@object-refinery.com>
2013-07-30 12:49:50 +0200

    Added Javadocs.

--- src/main/java/org/jfree/graphics2d/pdf/GraphicsStateDictionary.java
+++ src/main/java/org/jfree/graphics2d/pdf/GraphicsStateDictionary.java
@@ -35,23 +35,48 @@ public class GraphicsStateDictionary extends DictionaryObject {
     
     private float nonStrokeAlpha;
     
+    /**
+     * Creates a new instance with the type <code>/ExtGState</code>.
+     * 
+     * @param number  the PDF object number.
+     */
     public GraphicsStateDictionary(int number) {
         super(number, "/ExtGState");
     }
     
+    /**
+     * Returns the stroke alpha.
+     * 
+     * @return The stroke alpha. 
+     */
     public float getStrokeAlpha() {
         return this.strokeAlpha;   
     }
     
+    /**
+     * Sets the stroke alpha.
+     * 
+     * @param alpha  the stroke alpha. 
+     */
     public void setStrokeAlpha(float alpha) {
         this.strokeAlpha = alpha;
         this.dictionary.put("/CA", alpha);
     }
     
+    /**
+     * Returns the non-stroke alpha.
+     * 
+     * @return The non-stroke alpha. 
+     */
     public float getNonStrokeAlpha() {
         return this.nonStrokeAlpha;   
     }
     
+    /**
+     * Sets the non-stroke alpha.
+     * 
+     * @param alpha  the new alpha value. 
+     */
     public void setNonStrokeAlpha(float alpha) {
         this.nonStrokeAlpha = alpha;
         this.dictionary.put("/ca", alpha);
