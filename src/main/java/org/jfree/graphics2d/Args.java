94df52377fba347a235ff679a21da30de6ef4a6f
David Gilbert <david.gilbert@object-refinery.com>
2013-07-30 14:27:02 +0200

    Add Javadocs and private constructor.

--- src/main/java/org/jfree/graphics2d/Args.java
+++ src/main/java/org/jfree/graphics2d/Args.java
@@ -27,16 +27,37 @@
 package org.jfree.graphics2d;
 
 /**
- * Checks for argument validity.
+ * A utility class that performs checks for method argument validity.
  */
 public class Args {
 
+    private Args() {
+        // no need to instantiate this
+    }
+ 
+    /**
+     * Checks that an argument is non-<code>null</code> and throws an 
+     * <code>IllegalArgumentException</code> otherwise.
+     * 
+     * @param obj  the object to check for <code>null</code>.
+     * @param ref  the text name for the parameter (to include in the exception
+     *     message).
+     */
     public static void nullNotPermitted(Object obj, String ref) {
         if (obj == null) {
             throw new IllegalArgumentException("Null '" + ref + "' argument.");
         }
     }
     
+    /**
+     * Checks an array to ensure it has the correct length and throws an
+     * <code>IllegalArgumentException</code> if it does not.
+     * 
+     * @param length  the required length.
+     * @param array  the array to check.
+     * @param ref  the text name of the array parameter (to include in the 
+     *     exception message).
+     */
     public static void arrayMustHaveLength(int length, double[] array, 
             String ref) {
         nullNotPermitted(array, "array");
