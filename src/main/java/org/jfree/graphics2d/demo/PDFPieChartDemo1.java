4bdf23f2ca0eb2167fabe21da7f21c9e388e7e38
David Gilbert <david.gilbert@object-refinery.com>
2013-09-01 12:34:27 +0200

    Update to newer pie chart.

--- src/main/java/org/jfree/graphics2d/demo/PDFPieChartDemo1.java
+++ src/main/java/org/jfree/graphics2d/demo/PDFPieChartDemo1.java
@@ -26,17 +26,27 @@
 
 package org.jfree.graphics2d.demo;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Point;
+import java.awt.RadialGradientPaint;
 import java.awt.Rectangle;
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.IOException;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.title.TextTitle;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
 import org.jfree.graphics2d.pdf.PDFDocument;
 import org.jfree.graphics2d.pdf.PDFGraphics2D;
 import org.jfree.graphics2d.pdf.Page;
+import org.jfree.ui.HorizontalAlignment;
+import org.jfree.ui.RectangleEdge;
 
 /**
  * A demo/test for a pie chart.
@@ -45,17 +55,17 @@ public class PDFPieChartDemo1 {
     
     /**
      * Creates a sample dataset.
+     * 
+     * Source: http://www.bbc.co.uk/news/business-15489523
      *
      * @return A sample dataset.
      */
     private static PieDataset createDataset() {
         DefaultPieDataset dataset = new DefaultPieDataset();
-        dataset.setValue("One", new Double(43.2));
-        dataset.setValue("Two", new Double(10.0));
-        dataset.setValue("Three", new Double(27.5));
-        dataset.setValue("Four", new Double(17.5));
-        dataset.setValue("Five", new Double(11.0));
-        dataset.setValue("Six", new Double(19.4));
+        dataset.setValue("Samsung", new Double(27.8));
+        dataset.setValue("Others", new Double(55.3));
+        dataset.setValue("Nokia", new Double(16.8));
+        dataset.setValue("Apple", new Double(17.1));
         return dataset;
     }
 
@@ -69,18 +79,74 @@ public class PDFPieChartDemo1 {
     private static JFreeChart createChart(PieDataset dataset) {
 
         JFreeChart chart = ChartFactory.createPieChart(
-            "Pie Chart Demo 1",  // chart title
-            dataset,             // data
-            true,                // include legend
-            true,
-            false
+            "Smart Phones Manufactured / Q3 2011",  // chart title
+            dataset,            // data
+            false,              // no legend
+            true,               // tooltips
+            false               // no URL generation
         );
 
+        // set a custom background for the chart
+        chart.setBackgroundPaint(new GradientPaint(new Point(0, 0), 
+                new Color(20, 20, 20), new Point(400, 200), Color.DARK_GRAY));
+
+        // customise the title position and font
+        TextTitle t = chart.getTitle();
+        t.setHorizontalAlignment(HorizontalAlignment.LEFT);
+        t.setPaint(new Color(240, 240, 240));
+        t.setFont(new Font("Arial", Font.BOLD, 26));
+
         PiePlot plot = (PiePlot) chart.getPlot();
-        plot.setSectionOutlinesVisible(false);
-        plot.setNoDataMessage("No data available");
+       
+        plot.setBackgroundPaint(null);
+        plot.setInteriorGap(0.04);
+        plot.setOutlineVisible(false);
+        plot.setShadowPaint(null);
+        plot.setLabelShadowPaint(null);
+        
+        // use gradients and white borders for the section colours
+        plot.setSectionPaint("Others", createGradientPaint(new Color(200, 200, 255), Color.BLUE));
+        plot.setSectionPaint("Samsung", createGradientPaint(new Color(255, 200, 200), Color.RED));
+        plot.setSectionPaint("Apple", createGradientPaint(new Color(200, 255, 200), Color.GREEN));
+        plot.setSectionPaint("Nokia", createGradientPaint(new Color(200, 255, 200), Color.YELLOW));
+        plot.setBaseSectionOutlinePaint(Color.WHITE);
+        plot.setSectionOutlinesVisible(true);
+        BasicStroke bs = new BasicStroke(2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 1.0f);
+        plot.setBaseSectionOutlineStroke(new BasicStroke(2.0f));
+
+        // customise the section label appearance
+        plot.setLabelFont(new Font("Courier New", Font.BOLD, 20));
+        plot.setLabelLinkPaint(Color.WHITE);
+        plot.setLabelLinkStroke(new BasicStroke(2.0f));
+        plot.setLabelOutlineStroke(null);
+        plot.setLabelPaint(Color.WHITE);
+        plot.setLabelBackgroundPaint(null);
+        
+        // add a subtitle giving the data source
+        TextTitle source = new TextTitle("Source: http://www.bbc.co.uk/news/business-15489523", 
+                new Font("Courier New", Font.PLAIN, 12));
+        source.setPaint(Color.WHITE);
+        source.setPosition(RectangleEdge.BOTTOM);
+        source.setHorizontalAlignment(HorizontalAlignment.RIGHT);
+        chart.addSubtitle(source);
         return chart;
 
+    }
+
+    /**
+     * A utility method for creating gradient paints.
+     * 
+     * @param c1  color 1.
+     * @param c2  color 2.
+     * 
+     * @return A radial gradient paint.
+     */
+    private static RadialGradientPaint createGradientPaint(Color c1, Color c2) {
+        Point2D center = new Point2D.Float(0, 0);
+        float radius = 200;
+        float[] dist = {0.0f, 1.0f};
+        return new RadialGradientPaint(center, radius, dist,
+                new Color[] {c1, c2});
     }
 
     /**
@@ -93,7 +159,7 @@ public class PDFPieChartDemo1 {
     public static void main(String[] args) throws IOException {
         JFreeChart chart = createChart(createDataset());
         PDFDocument pdfDoc = new PDFDocument();
-        pdfDoc.setTitle("PDFBarChartDemo1");
+        pdfDoc.setTitle("PDFPieChartDemo1");
         pdfDoc.setAuthor("Object Refinery Limited");
         Page page = pdfDoc.createPage(new Rectangle(612, 468));
         PDFGraphics2D g2 = page.getGraphics2D();
