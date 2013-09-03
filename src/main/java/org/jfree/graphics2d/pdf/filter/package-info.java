dd2a478d12a9b7d96a36f90f246cfc4ca81d1204
David Gilbert <david.gilbert@object-refinery.com>
2013-09-03 13:30:02 +0200

    Deleted PDF classes.

--- src/main/java/org/jfree/graphics2d/pdf/filter/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Filters that support the {@link org.jfree.graphics2d.pdf.PDFGraphics2D} 
- * implementation.
- */
-package org.jfree.graphics2d.pdf.filter;
--- src/main/java/org/jfree/graphics2d/pdf/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains {@link org.jfree.graphics2d.pdf.PDFGraphics2D} and supporting 
- * classes to allow Java2D rendering to Adobe's Portable Document Format (PDF).
- */
-package org.jfree.graphics2d.pdf;
