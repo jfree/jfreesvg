53493b29e6dc64f290b667c20f7bbf6f6e1f2c0e
David Gilbert <david.gilbert@object-refinery.com>
2013-09-12 15:51:57 +0200

    Write to SVG file rather than HTML.

--- src/main/java/org/jfree/graphics2d/demo/SVGBarChartDemo1.java
+++ src/main/java/org/jfree/graphics2d/demo/SVGBarChartDemo1.java
@@ -139,7 +139,7 @@ public class SVGBarChartDemo1 {
         SVGGraphics2D g2 = new SVGGraphics2D(600, 400);
         Rectangle r = new Rectangle(0, 0, 600, 400);
         chart.draw(g2, r);
-        File f = new File("SVGBarChartDemo1.html");
-        SVGUtils.writeToHTML(f, "SVGBarChartDemo1", g2.getSVGElement());
+        File f = new File("SVGBarChartDemo1.svg");
+        SVGUtils.writeToSVG(f, g2.getSVGElement());
     }
 }
