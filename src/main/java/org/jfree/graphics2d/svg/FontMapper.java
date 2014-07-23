35fc60daa8427ffe675ec6c7aee16d8ffbca876e
David Gilbert <david.gilbert@object-refinery.com>
2014-07-23 10:04:13 +0200

    Change license from AGPL to GPL.

--- src/main/java/org/jfree/graphics2d/svg/FontMapper.java
+++ src/main/java/org/jfree/graphics2d/svg/FontMapper.java
@@ -7,22 +7,22 @@
  * Project Info:  http://www.jfree.org/jfreesvg/index.html
  * 
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
  * Other names may be trademarks of their respective owners.]
  * 
- * If you do not wish to be bound by the terms of the AGPL, an alternative
+ * If you do not wish to be bound by the terms of the GPL, an alternative
  * commercial license can be purchased.  For details, please see visit the
  * JFreeSVG home page:
  * 
