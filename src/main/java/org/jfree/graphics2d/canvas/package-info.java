9edba39b878a4b34a3644aebab1a2dff59b7d0d9
David Gilbert <dave@jfree.org>
2015-05-23 09:35:55 +0200

    Remove Canvas code...focus on SVG only in this package

--- src/main/java/org/jfree/graphics2d/canvas/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains {@link org.jfree.graphics2d.canvas.CanvasGraphics2D}  to allow 
- * Java2D rendering to a JavaScript function that will render the vector 
- * graphics on an HTML5 Canvas.
- */
-package org.jfree.graphics2d.canvas;
