66b607e24fca332869134efc19c9d12ba27157e0
David Gilbert <dave@jfree.org>
2015-05-23 09:34:00 +0200

    Add GPL header to test file.

--- src/test/java/org/jfree/graphics2d/TestGraphics2D.java
+++ src/test/java/org/jfree/graphics2d/TestGraphics2D.java
@@ -1,8 +1,35 @@
-/**
- * (C)opyright 2013-2015, by Object Refinery Limited.  All rights reserved.
+/* ===================================================
+ * JFreeSVG : an SVG library for the Java(tm) platform
+ * ===================================================
  * 
- * This test file is internal, it will not be released.
+ * (C)opyright 2013-2015, by Object Refinery Limited.  All rights reserved.
+ *
+ * Project Info:  http://www.jfree.org/jfreesvg/index.html
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ * 
+ * If you do not wish to be bound by the terms of the GPL, an alternative
+ * commercial license can be purchased.  For details, please see visit the
+ * JFreeSVG home page:
+ * 
+ * http://www.jfree.org/jfreesvg
+ * 
  */
+
 package org.jfree.graphics2d;
 
 import java.awt.BasicStroke;
