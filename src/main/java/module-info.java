6581c7b716542ae042cda91d3d454ae14d110d48
David Gilbert <dave@jfree.org>
2021-01-10 11:14:50 +0100

    Add Javadoc comment.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,7 @@
+/**
+ * JFreeSVG is a Java module that enables the generation of Scalable Vector Graphics (SVG) output via the
+ * standard Java2D API.  The project is hosted at GitHub: <a href="https://github.com/jfree/jfreesvg">https://github.com/jfree/jfreesvg</a>.
+ */
 module org.jfree.svg {
     requires java.desktop;
     requires java.logging;
