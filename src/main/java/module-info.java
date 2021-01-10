594f20d91edb7882d95b6434654fdb9884b10657
David Gilbert <dave@jfree.org>
2021-01-10 11:48:19 +0100

    Update module Javadocs.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,11 @@
 /**
  * JFreeSVG is a Java module that enables the generation of Scalable Vector Graphics (SVG) output via the
- * standard Java2D API.  The project is hosted at GitHub: <a href="https://github.com/jfree/jfreesvg">https://github.com/jfree/jfreesvg</a>.
+ * standard Java2D API.  For example, the following chart is created with Orson Charts and rendered with JFreeSVG:
+ * <br><br>
+ * <img src="doc-files/chart.svg" alt="chart.svg">
+ * <br><br>
+ * The project is hosted at GitHub: <a href="https://github.com/jfree/jfreesvg">https://github.com/jfree/jfreesvg</a>.
+ * @see <a href="https://jfree.github.io/jfreechart-and-opencsv/">https://jfree.github.io/jfreechart-and-opencsv/</a>
  */
 module org.jfree.svg {
     requires java.desktop;
