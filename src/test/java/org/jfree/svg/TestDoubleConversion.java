c2e98321e40ea4d6b93f38070abd20e5b34d1dcf
David Gilbert <dave@jfree.org>
2021-05-23 11:10:25 +0200

    Update copyright headers.

--- src/test/java/org/jfree/svg/TestDoubleConversion.java
+++ src/test/java/org/jfree/svg/TestDoubleConversion.java
@@ -32,8 +32,8 @@
 
 package org.jfree.svg;
 
-import java.util.Locale;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Locale;
 import org.junit.jupiter.api.Test;
 import java.util.function.DoubleFunction;
 
